fc5d92fe7166d00a78b9262398650bd668b66de1
cbyzz24 <155403160+cbyzz24@users.noreply.github.com>
2025-04-10 19:29:01 -0600

    Restyled layout and added functionality

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-// alert("CounterApp");
+const outputDisplayEl = document.getElementById("outputDisplay")
+const minusBtnEl = document.getElementById("minus")
+const resetBtnEl = document.getElementById("reset")
+const plusBtnEl = document.getElementById("plus")
+
+let count = "0"
+
+// minusBtnEl.addEventListener("click", function(e){
+//     alert("Minus 1")
+// })
+// minusBtnEl.addEventListener("click", (e) =>{
+//     alert('Subtract 1')
+// })
+minusBtnEl.addEventListener("click", (e) =>{
+    outputDisplayEl.innerText = count--
+})
+
+resetBtnEl.addEventListener("click", (e) =>{
+    outputDisplayEl.innerText = "0"
+})
+
+plusBtnEl.addEventListener("click", (e) =>{
+    outputDisplayEl.innerText = count++
+})
