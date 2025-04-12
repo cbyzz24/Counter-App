605b4e328200184494f82b63850836ae670a49db
Christopher Belcher <cpbelcher1@gmail.com>
2025-04-12 13:23:17 -0600

    Enhanced style & script

--- main.js
+++ main.js
@@ -1,44 +1,21 @@
-const outputDisplayEl = document.getElementById("outputDisplay");
+const displayEl = document.getElementById("outputDisplay");
 const minusBtnEl = document.getElementById("minus");
 const resetBtnEl = document.getElementById("reset");
 const plusBtnEl = document.getElementById("plus");
 
 let count = 0;
 
-// minusBtnEl.addEventListener("click", function(e){
-//     alert("Minus 1")
-// })
-// minusBtnEl.addEventListener("click", (e) =>{
-//     alert('Subtract 1')
-// })
-
-// minusBtnEl.onclick = function(){
-//     count--;
-//     outputDisplayEl.textContent = count;
-// }
-
-// resetBtnEl.onclick = function(){
-//     count = 0;
-//     outputDisplayEl.textContent = count;
-// }
-
-// plusBtnEl.onclick = function(){
-//     count++;
-//     outputDisplayEl.textContent = count;
-// }
-
-minusBtnEl.addEventListener("click", (e) =>{
+minusBtnEl.addEventListener("click", () =>{
     count--
-    outputDisplayEl.textContent = count
+    displayEl.textContent = count
 })
 
-resetBtnEl.addEventListener("click", (e) =>{
+resetBtnEl.addEventListener("click", () =>{
     count = 0
-    outputDisplayEl.textContent = count
+    displayEl.textContent = 0
 })
 
-plusBtnEl.addEventListener("click", (e) =>{
+plusBtnEl.addEventListener("click", () =>{
     count++
-    outputDisplayEl.textContent = count 
+    displayEl.textContent = count
 })
-
